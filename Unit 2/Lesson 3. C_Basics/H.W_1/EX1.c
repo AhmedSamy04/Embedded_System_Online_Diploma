b48f13533292126013e95cc012427345953318af
AhmedSamy04 <87011921+AhmedSamy04@users.noreply.github.com>
2023-08-02 16:42:19 +0300

    Add files via upload

---
+++ Unit 2/Lesson 3. C_Basics/H.W_1/EX1.c
@@ -0,0 +1,20 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Aug 1, 2023
+ *      Author: Ahmed Sami
+ */
+
+
+
+// Write C Program to Print a Sentence
+
+
+
+#include "stdio.h"
+
+void main ()
+{
+	printf ("C Programming");
+
+}
---
+++ Unit 2/Lesson 3. C_Basics/H.W_1/EX7.c
@@ -0,0 +1,26 @@
+/*
+ * EX7.c
+ *
+ *  Created on: Aug 2, 2023
+ *      Author: Ahmed Sami
+ */
+
+
+#include "stdio.h"
+
+void main()
+{
+float a, b;
+printf("Enter value of a: ");
+fflush(stdin);     fflush(stdout);
+scanf("%f", &a);
+printf("Enter value of b: ");
+fflush(stdin);     fflush(stdout);
+scanf("%f", &b);
+a =a + b;
+b = a - b;
+a = a - b;
+printf("After swapping, value of a = %.2f \n", a);
+printf("After swapping, value of b = %.2f \n", b);
+
+}
