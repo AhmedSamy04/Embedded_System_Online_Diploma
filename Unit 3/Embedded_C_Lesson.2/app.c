17d52205c05932c68e38b5be73113ae06e7bce95
AhmedSamy04 <ahmed31009532@f-eng.tanta.edu.eg>
2023-10-26 18:44:49 +0300

    modifications

--- Unit 3/Embedded_C_Lesson.2/app.c
+++
@@ -1,9 +0,0 @@
-#include "Uart.h"
-
-unsigned char string_buffer[100] = "learn-in-depth: Galal";
-unsigned const char string_buffer2[100] = "to create a rodata section";
-
-void main(void)
-{
-	Uart_send_string(string_buffer);
-}
--- Unit 3/Embedded_C_Lesson.2/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART_H_
-#define UART_H_
-
-#define UART0DR *(volatile unsigned int* const)(0x101f1000)
-
-void Uart_send_string(unsigned char *P_tx_string);
-
-#endif /* UART_H_ */
